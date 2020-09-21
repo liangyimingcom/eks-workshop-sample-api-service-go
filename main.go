51266b0c2fc1d21205b41c2ae9f053f6fb6dad2f
liangyimingcom <32609181+liangyimingcom@users.noreply.github.com>
2020-09-21 18:38:34 +0800

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World, hello Yiming"}
+		res := &response{Message: "Hello World, hello Yiming, today 21,sep"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
